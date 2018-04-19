5696b155c70bff1a6ff32bde0449b5159031f76f
Richard Wiggins <richard@pixelcreation.co.uk>
2018-04-19 18:03:56 +0100

    Initial commit.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,7 +2,26 @@ let mix = require('laravel-mix');
 let tailwindcss = require('tailwindcss');
 
 mix.sass('./app.scss', './public/css')
-  .options({
-    processCssUrls: false,
-    postCss: [ tailwindcss('./tailwind.js') ],
-  });
+	.options({
+		processCssUrls: false,
+		postCss: [ tailwindcss('./tailwind.js') ],
+	});
+
+mix.browserSync({
+		proxy: 'pwic.local',
+		files: [
+            './public/**/*.+(html|php)'
+        ],
+	})
+
+//let buildPath = 'build/'
+
+
+// mix.setPublicPath('public')
+  // .browserSync({
+  //   proxy: 'pwic.local/',
+  //   files: [
+  //     // 'craft/templates/**/*.twig'
+  //     './public/*.html'
+  //   ]
+  // })
